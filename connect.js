d5afd8b39c0bfee1c0fe5fc394ccf0fa4ca57a2d
Steven Medina <stevenmedina@ufl.edu>
2019-10-19 16:41:31 -0400

    Trying to connect to docker db attempt 2

--- connect.js
+++ connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://user1:password1@127.0.0.1:27017/test_db', { useNewUrlParser: true }).then(() => {
+mongoose.connect('mongodb://user1:password1@127.0.0.1:27017/ppa2_db', { useNewUrlParser: true }).then(() => {
     console.log('Database connected...');
 })
