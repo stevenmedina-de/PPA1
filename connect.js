a719b2a76d63c958467243f85cd700bae73e5a73
Steven Medina <stevenmedina@ufl.edu>
2019-10-19 16:52:20 -0400

    connecting to db attempt 3

--- connect.js
+++ connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://user1:password1@127.0.0.1:27017/ppa2_db', { useNewUrlParser: true }).then(() => {
-    console.log('Database connected...');
-})
+mongoose.connect('mongodb://user1:password1@127.0.0.1:27017/ppa2_db', { useNewUrlParser: true })
+    .then(() => console.log('Database connected...'))
+    .catch((err) => console.err(err))
