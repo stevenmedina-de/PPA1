aad62883bab2c878ef9a4569a8d1d3979d8e0178
Steven Medina <stevenmedina@ufl.edu>
2019-10-20 13:15:46 -0400

    commit

--- shortestDistanceDB.js
+++ shortestDistanceDB.js
@@ -3,3 +3,9 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://master:password0@ds129233.mlab.com:29233/ppa2', { useNewUrlParser: true })
     .then(() => console.log('Database connected...'))
     .catch((err) => console.err(err))
+
+const shortestDistanceSchema = new mongoose.Schema({
+    Date: Date.now,
+    inputs: [Number],
+    outputs: Number
+})
